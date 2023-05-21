105bdaffd06f429d3aff9c8e8058e77bb6507a55
verveguy <bpjadam@gmail.com>
2023-05-21 09:26:05 -0400

    Ignore content underneath top level node when finding inline refs

--- src/services/inlinerefs.py
+++ src/services/inlinerefs.py
@@ -7,9 +7,15 @@ router = APIRouter()
 
 @router.post("/inlinerefs", response_class=HTMLResponse)
 def inlinerefs(req: NodeRequest, response: Response):
+  if not req.context:
+    response.status_code=status.HTTP_204_NO_CONTENT
+    return None    
+  
   # find all the refs in the context
   pattern = re.compile(r'(\[\[[^\]]*\]\])')
-  matches = re.findall(pattern, req.context)
+  # just use the first line, since we only want the primary node
+  first = req.context.partition('\n')[0]
+  matches = re.findall(pattern, first)
 
   if len(matches) > 0:
     result = ''.join(["- inline ref::"+id+"\n" for id in matches])
