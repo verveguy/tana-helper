7337ce83ec4628783a0df75b2de6113ff8b87e11
verveguy <bpjadam@gmail.com>
2023-11-18 19:10:21 -0500

    Arch and Version in app name

--- release/build.sh
+++ release/build.sh
@@ -1,33 +1,31 @@
 #!/bin/sh
 
 # first, build the webapp. It will push its artifacts to the service
-(cd ../webapp; ./build.sh)
+#(cd ../webapp; ./build.sh)
 
 # now build the service and the app wrapper
-(cd ../service/; ./build.sh)
+#(cd ../service/; ./build.sh)
+
+OS_VERSION=$(sw_vers -productVersion | awk -F '.' '{print $1 "." $2}')
+ARCH=$(uname -m)
+NAME="Tana Helper ($OS_VERSION-$ARCH)"
 
 # Create a folder (named dmg) to prepare our DMG in (if it doesn't already exist).
-test -f "dist" && rm -r "dist"
-mkdir -p dist/dmg
-
-# Empty the dmg folder.
-rm -r dist/dmg/*
+rm -rf "dist"
+mkdir -p "dist/dmg/$NAME.app"
 
 # Copy the app bundle to the dmg folder.
-cp -r "../service/dist/Tana Helper.app" dist/dmg
-
-# If the DMG already exists, delete it.
-test -f "dist/Tana Helper.dmg" && rm "dist/Tana Helper.dmg"
+ditto '../service/dist/Tana Helper.app' "dist/dmg/$NAME.app"
 
 # Create the DMG.
 create-dmg \
-  --volname "Tana Helper" \
+  --volname "$NAME" \
   --volicon "Tana Helper.icns" \
   --window-pos 200 120 \
   --window-size 600 300 \
   --icon-size 100 \
-  --icon "Tana Helper.app" 175 120 \
-  --hide-extension "Tana Helper.app" \
+  --icon "$NAME.app" 175 120 \
+  --hide-extension "$NAME.app" \
   --app-drop-link 425 120 \
-  "dist/Tana Helper.dmg" \
+  "dist/$NAME.dmg" \
   "dist/dmg/"
