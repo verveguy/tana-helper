a53e188c77c7ab46b7790483c7029abd886cef93
verveguy <bpjadam@gmail.com>
2023-12-17 11:05:51 -0500

    Parallelize all builds
    Normalize build outputs and clean up logging

--- release/build.sh
+++ release/build.sh
@@ -21,17 +21,14 @@ if [[ "$OSTYPE" == "darwin"* ]]; then
     ARCH=$(uname -m)
     NAME="Tana Helper ($OS_VERSION-$ARCH)"
     
-    # Create a folder (named dmg) to prepare our DMG in (if it doesn't already exist).
-    mkdir -p "dist/dmg/$NAME.app"
-
-    # Copy the app bundle to the dmg folder.
-    ditto '../service/dist/Tana Helper.app' "dist/dmg/$NAME.app"
+    mkdir -p "dist/$NAME.app"
+    ditto '../service/dist/Tana Helper.app' "dist/$NAME.app"
     echo "Mac .app bundle done"
 elif [[ "$OSTYPE" == "msys"* ]]; then
-    # Windows bundle done
-    mkdir -p "dist/win"
-    # move files from service/dist to dist/win/
-    # mv ../service/dist/tanahelper dist/win/
-    tar -cvzf dist/win/tanahelper.tar.gz ../service/dist/tanahelper > /dev/null
-    echo "Windows bundle done"
+    OS_VERSION="11"
+    ARCH="win"
+    NAME="Tana Helper ($OS_VERSION-$ARCH)"
+    mkdir -p "dist"
+    tar -cvzf "dist/$NAME.tar.gz" ../service/dist/tanahelper > /dev/null
+    echo "Windows tarball done"
 fi
