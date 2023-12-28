6215f64504b04b9131aee56c2c4e8e08cd9a85d1
verveguy <bpjadam@gmail.com>
2023-12-27 21:28:38 -0500

    Fixup switch_branch to understand new branches

--- release/switch_branch.sh
+++ release/switch_branch.sh
@@ -12,6 +12,8 @@ hosts=("Monterey-x86" "Monterey-arm" "windows-x86")
 # Loop through each host
 for host in "${hosts[@]}"
 do
+  echo "Configuring $host"
+  git push $host $1
   # SSH into the host and switch branches
   ssh "$host" "cd ~/dev/tana/tana-helper/release && git fetch && git checkout $1"
 done
