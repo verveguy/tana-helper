75e46ee70ea28f4efe52949747864aaad3a17477
verveguy <bpjadam@gmail.com>
2024-01-10 20:02:12 -0500

    Remove mistral. Bad merge

--- service/service/main.py
+++ service/service/main.py
@@ -5,7 +5,7 @@ from fastapi.staticfiles import StaticFiles
 from fastapi.responses import FileResponse
 from logging import getLogger
 # note pinecone is not included - problems with PyInstaller and google API depedencies...
-from service.endpoints import calendar, chroma, weaviate, mistral, inlinerefs, exec_code, webhooks, jsonify, graph_view, class_diagram, configure, proxy, topics
+from service.endpoints import calendar, chroma, weaviate, inlinerefs, exec_code, webhooks, jsonify, graph_view, class_diagram, configure, proxy, topics
 from service.logconfig import setup_rich_logger
 from snowflake import SnowflakeGenerator
 import time
@@ -62,7 +62,6 @@ app.include_router(class_diagram.router)
 app.include_router(topics.router)
 app.include_router(configure.router)
 app.include_router(proxy.router)
-app.include_router(mistral.router)
 
 @app.middleware("http")
 async def add_get_authorization_headers(request: Request, call_next):
