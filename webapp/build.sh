a32a6926b3fbe72a5d2f7dcef5061e9a9861d345
verveguy <bpjadam@gmail.com>
2024-08-22 12:21:59 -0400

    Fix for build from clean checkout

--- webapp/build.sh
+++ webapp/build.sh
@@ -4,8 +4,3 @@ set -euo pipefail # return error if any command fails
 yarn install
 
 yarn build
-
-# push build artifacts to service for packaging
-rm -rf ../service/service/dist
-mkdir -p ../service/service/dist
-cp -rp dist/* ../service/service/dist
